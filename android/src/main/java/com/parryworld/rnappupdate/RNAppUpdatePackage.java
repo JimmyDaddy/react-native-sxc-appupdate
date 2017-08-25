3a091050e6d709099bc64a3360daa9516132b0ea
JimmyDaddy <heyjimmygo@gmail.com>
2017-08-25 20:28:31 +0800

    适配最新版RN

--- android/src/main/java/com/parryworld/rnappupdate/RNAppUpdatePackage.java
+++ android/src/main/java/com/parryworld/rnappupdate/RNAppUpdatePackage.java
@@ -24,11 +24,6 @@ public class RNAppUpdatePackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
